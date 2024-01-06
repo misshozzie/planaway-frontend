362bacc893b6e7b8772c367d4ecddb93deb0b187
ivyzhang <xiaowen_zhang@outlook.com>
2024-01-06 11:29:37 +0800

    add chakra

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
   </React.StrictMode>
 );
