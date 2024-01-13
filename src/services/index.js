694f5c4ca4bb4a69f3a45f8a094d0c1fd10a96f4
Hosanna Martin <vivahosanna@icloud.com>
2024-01-14 00:32:52 +0800

    create validity store data handle error

---
+++ src/services/index.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const createBackendServer = (baseURL) => {
+  const api = axios.create({
+    baseURL: `${baseURL}api/`,
+    withCredentials: true,
+    headers: {
+      Accept: "application/json",
+    },
+    timeout: 60 * 1000,
+  });
+
+  /*=== INTERCEPTOR ===*/
+  // checkpoint
+  //let successful responses pass through
+  // and handle errors
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      const message = error?.response?.data?.message;
+      error.message = message ?? error.message;
+      if (error?.response?.data?.errors)
+        error.errors = error?.response?.data?.errors;
+      return Promise.reject(error);
+    }
+  );
+
+  /*=== POST REQUEST ===*/
+  const authLogin = async (body) => api.post("auth/login", body);
+  const authLogout = async () => api.post("auth/logout");
+  const authSignUp = async (body) => api.post("auth/register", body);
+
+  /*=== GET REQUEST ===*/
+  const updateProfile = async ({ id, body }) =>
+    api.put(`auth/update/${id}`, body);
+
+  return {
+    authLogin,
+    authSignUp,
+    authLogout,
+    updateProfile,
+  };
+};
+
+const apis = createBackendServer("http://localhost:3000/");
+
+export default apis;
