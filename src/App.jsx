109e731a88d2ed4ac184b69063ddde75c162d74d
ivyzhang <xiaowen_zhang@outlook.com>
2024-01-13 11:27:15 +0800

    refactor routes and update api call to a custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TripPage from "./pages/TripPage";
 import PlanPage from "./pages/PlanPage";
+import NewTripForm from "./components/NewTripForm";
 
 import "./App.css";
 
@@ -10,8 +11,11 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/:username" element={<TripPage />} />
-        <Route path="/:username/:tripid" element={<PlanPage />} />
+        <Route path="user/trips">
+          <Route index={true} element={<TripPage />} />
+          <Route path="new" element={<NewTripForm />} />
+        </Route>
+        <Route path="user/trips/plans" element={<PlanPage />} />
       </Routes>
     </>
   );
