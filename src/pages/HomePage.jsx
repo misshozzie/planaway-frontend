b7306ecd092c3dca405bd3a854022a31ca88a8fb
Hosanna Martin <vivahosanna@icloud.com>
2024-01-13 19:09:27 +0800

    Create a NavBar and update HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,75 @@
-//Home page for login & signup
-export default function HomePage() {
+//for login and signup
+import React, { useEffect } from "react";
+import { Box, Flex, Heading, Text, Button, useColorModeValue } from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaSignInAlt, FaUserPlus } from "react-icons/fa";
+import Cookies from "js-cookie";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const userCookie = Cookies.get("user");
+    if (userCookie) {
+      navigate("/trips");
+    }
+  }, []);
+
   return (
     <>
-      <p>Home Page</p>
+      <Flex
+        align="center"
+        justify="center"
+        height="100vh"
+        bgSize="cover"
+        bgPosition="center"
+      >
+        <Box
+          minWidth="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          boxShadow="lg"
+          p={8}
+          bg="useColorModeValue('rgba(255, 255, 255, 0.8)', 'rgba(0, 0, 0, 0.8)')"
+          color={useColorModeValue("black", "white")}
+          textAlign="center"
+        >
+          <Heading
+            mb={6}
+            fontSize="5xl"
+            fontWeight="bold"
+            color={useColorModeValue("teal.600", "teal.300")}
+          >
+            Planaway
+          </Heading>
+          <Text mb={6} fontSize="xl">
+            welcome to planaway
+          </Text>
+          <Link to="/login">
+            <Button
+              mb={4}
+              colorScheme="#DBCC95"
+              width="full"
+              size="lg"
+              bg="#CD8D7A"
+              _hover="#DBCC95"
+            >
+              LOGIN
+            </Button>
+          </Link>
+          <Link to="/sign-up">
+            <Button
+              colorScheme="blue"
+              width="full"
+              size="lg"
+              bg="#CD8D7A"
+              _hover="#DBCC95"
+            >
+              SIGNUP
+            </Button>
+          </Link>
+        </Box>
+      </Flex>
     </>
   );
-}
+};
+export default HomePage;
