46422a97289770b4d6477de2fa69864be4438b0b
Cheryl Tan <cheryltan134@gmail.com>
2024-01-16 22:01:53 +0800

    fe for plans

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-route
 import HomePage from "./pages/HomePage";
 import TripPage from "./pages/TripPage";
 import PlanPage from "./pages/PlanPage";
+import PlanCard from './components/PlanCard';
 import NewTripForm from "./components/NewTripForm";
 import SignUp from "./pages/SignUp";
+import NewPlanForm from "./components/NewPlanForm"
+import UpdatePlanForm from "./components/UpdatePlanForm"
 import ProfileUpdate from "./components/ProfileUpdateForm";
 import Login from "./components/LoginForm";
 import "./index.css";
@@ -20,7 +23,9 @@ function App() {
           <Route index={true} element={<TripPage />} />
           <Route path="new" element={<NewTripForm />} />
         </Route>
-        <Route path="user/trips/plans" element={<PlanPage />} />
+        <Route path="user/trips/plans/new" element={<NewPlanForm />} />
+        <Route path="user/trips/plans/update" element={<UpdatePlanForm />} />
+        <Route path="user/trips/plans/view" element={<PlanCard />} />
       </Routes>
     </>
   );
