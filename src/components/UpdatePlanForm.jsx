f9b40e515c3008fe7934b94db8751be6e62e5f74
Cheryl Tan <cheryltan134@gmail.com>
2024-01-23 20:48:03 +0800

    added placeholder

--- src/components/UpdatePlanForm.jsx
+++ src/components/UpdatePlanForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, Link } from 'react-router-dom';
 import {
   Box,
@@ -8,26 +8,24 @@ import {
   Input,
   Button,
   Image,
-  Heading, 
-  Spinner,
+  Heading,
   Textarea,
-  Text,
-  useColorModeValue,
 } from "@chakra-ui/react";
 import logo from "../assets/PAlogo.png";
 import bg from "../assets/Planawaybg.png";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router";
-import { updatePlan } from "../api/plans"; 
+import { updatePlan, getOnePlan } from "../api/plans";
 
 export default function UpdateTripForm() {
-
-  const [formState, setFormState] = useState({})
+  const [formState, setFormState] = useState({
+    header: "",
+    description: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const { planid, tripid } = useParams(); 
+  const { planid, tripid } = useParams();
   const navigate = useNavigate();
-
 
   function handleChange(evt) {
     const { name, value } = evt.target;
@@ -36,100 +34,114 @@ export default function UpdateTripForm() {
       [name]: value,
     }));
   }
-  
+
+  useEffect(() => {
+    async function fetchPlanData() {
+      try {
+        const data = await getOnePlan(tripid, planid);
+        // Set formState directly from the fetched data
+        setFormState({
+          header: data.header,
+          description: data.description,
+        });
+      } catch (error) {
+        console.error("Error fetching plan data:", error);
+      }
+    }
+    fetchPlanData();
+  }, [planid, tripid]);
+
   async function handleSubmit(evt) {
     evt.preventDefault();
     try {
       const result = await updatePlan(formState, planid);
       setIsLoading(false);
-      // navigate("/user/trips"); // Redirect after successful submission
-    if (result.error) {
-      setError(result.error);
-    } else {
-      console.log('Plan has been updated');
-      navigate(`/user/trips/plans/${tripid}`);
-    } 
-  }catch (error) {
+      if (result.error) {
+        setError(result.error);
+      } else {
+        console.log('Plan has been updated');
+        navigate(`/user/trips/plans/${tripid}`);
+      }
+    } catch (error) {
       setIsLoading(false);
       console.error("Error submitting form:", error);
       // Handle error, show an error message, or retry submission
-      // NEED TO PROVIDE FEEDBACK TO THE USER 
+      // NEED TO PROVIDE FEEDBACK TO THE USER
     }
   }
- 
-  return (
 
+  return (
     <form onSubmit={handleSubmit}>
-    <Flex
-      align="center"
-      justify="center"
-      height="100vh"
-      direction="column"
-      style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover" }}
-    >
-      <Heading align="center">
-        <Image src={logo} alt="planaway" height={200} />
-      </Heading>
-      <br />
-      <br />
-      <Box
-        minWidth="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        boxShadow="lg"
-        bg="rgba(195, 226, 194, 0.30)"
-        w="700px"
-        h="300px"
-        p="32px"
-        textAlign="center"
-        zIndex="2"
+      <Flex
+        align="center"
+        justify="center"
+        height="100vh"
+        direction="column"
+        style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover" }}
       >
-    <div>
-      <Link to={`/user/trips/plans/${tripid}`}>
-        <ArrowLeftIcon />
-        Go Back
-      </Link>
-    </div>
-        <Heading as="h2" size="l" mt={4}>
-          UPDATE PLAN
+        <Heading align="center">
+          <Image src={logo} alt="planaway" height={200} />
         </Heading>
-        <FormControl isRequired>
-          <FormLabel>Header</FormLabel>
-          <Input
-            backgroundColor="white"
-            type="text"
-            placeholder="Enter your destination"
-            mb={4}
-            name="header"
-            onChange={handleChange}
-          />
-        </FormControl>
-  
-        <FormControl isRequired>
-          <FormLabel>Description</FormLabel>
-          <Textarea
-            backgroundColor="white"
-            type="text"
-            placeholder="Enter your plans"
-            mb={4}
-            name="description"
-            onChange={handleChange}
-          />
-        </FormControl>
         <br />
-        <Button
-          type="submit"
-          bg="#CD8D7A"
-          w="280px"
-          mt={4}
-          spinnerPlacement="start"
-          loadingText="Saving"
-          _hover={{ bg: " #DBCC95" }}
+        <br />
+        <Box
+          minWidth="sm"
+          borderWidth="1px"
+          borderRadius="lg"
+          boxShadow="lg"
+          bg="rgba(195, 226, 194, 0.30)"
+          w="700px"
+          h="300px"
+          p="32px"
+          textAlign="center"
+          zIndex="2"
         >
-          SAVE
-        </Button>
-      </Box>
-    </Flex>
-  </form>
-    );
-};
+          <div>
+            <Link to={`/user/trips/plans/${tripid}`}>
+              <ArrowLeftIcon />
+              Go Back
+            </Link>
+          </div>
+          <Heading as="h2" size="l" mt={4}>
+            UPDATE PLAN
+          </Heading>
+          <FormControl isRequired>
+            <FormLabel>Header</FormLabel>
+            <Input
+              backgroundColor="white"
+              type="text"
+              mb={4}
+              name="header"
+              value={formState.header}
+              onChange={handleChange}
+            />
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              backgroundColor="white"
+              type="text"
+              mb={4}
+              name="description"
+              value={formState.description}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <br />
+          <Button
+            type="submit"
+            bg="#CD8D7A"
+            w="280px"
+            mt={4}
+            spinnerPlacement="start"
+            loadingText="Saving"
+            _hover={{ bg: " #DBCC95" }}
+          >
+            SAVE
+          </Button>
+        </Box>
+      </Flex>
+    </form>
+  );
+}
