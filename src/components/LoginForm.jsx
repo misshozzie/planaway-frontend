3b2126851c2c18e30287fed8d5014206e78d4bba
Cheryl Tan <cheryltan134@gmail.com>
2024-01-26 17:23:31 +0800

    give password feedback to user

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -42,6 +42,8 @@ const LoginForm = ({ setUser }) => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [errors, setErrors] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -136,6 +138,12 @@ const LoginForm = ({ setUser }) => {
       // Baby step!
     } catch (e) {
       console.log(e);
+      if (e.message && e.message.includes("Invalid Login")) {
+        // Set the error message for "Invalid Login"
+        setErrorMessage("Password or email is incorrect");
+      } else {
+        setErrorMessage(e);
+      }
     }
   }
 
@@ -178,6 +186,11 @@ const LoginForm = ({ setUser }) => {
               LOG IN
             </Heading>
           <form>
+          {errorMessage && (
+          <Text color="red.500" mt={2}>
+            {errorMessage}
+          </Text>
+            )}
             {/* <Link as={RouterLink} to="/" display="flex" alignItems="center">
               <ArrowLeftIcon />
               Go Home
