dcab4d70cf2e41b5ea3aeffb875eabc93e4c909b
Hosanna Martin <vivahosanna@icloud.com>
2024-01-26 21:00:41 +0800

    logo asset typo

--- src/components/UpdatePlanForm.jsx
+++ src/components/UpdatePlanForm.jsx
@@ -12,7 +12,7 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import logo from "../assets/PAlogo.png";
-import bg from "../assets/Planawaybg.png";
+import bg from "../assets/planawaybg.png";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router";
 import { updatePlan, getOnePlan } from "../api/plans";
